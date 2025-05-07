3d16e0d77d35bf70f6a2f1d9eb73f485d08aa7ad
Ashish Shrestha <ashishshr27@gmail.com>
2025-05-07 17:39:26 +0545

    add decrypt function

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-choice = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-message = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
 def encrypt(message, shift):
   # print(message)
   # print(shift)
@@ -13,11 +9,37 @@ def encrypt(message, shift):
     shift_position = curr_position + shift 
     if shift_position >= 25:
       shift_position = shift_position % 26 
-    print(shift_position)
+    # print(shift_position)
     cipher_text += alphabets[shift_position]
   print(cipher_text)
 
-encrypt(message, shift)
+# encrypt(message, shift)
+
+def decrypt(cipher_text, shift):
+  message = ""
+  for char in cipher_text:
+    curr_position = alphabets.index(char)
+    shift_position = curr_position - shift 
+    if shift_position < 0:
+      shift_position = shift_position + 26 
+    # print(shift_position)
+    message += alphabets[shift_position]
+  print(message)
+
+# decrypt(message, shift)
+
+choice = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+
+if choice != "encode" and choice != "decode":
+  print("Invalid choice!")
+else:
+  message = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+
+  if choice == "encode":
+    encrypt(message, shift)
+  else: 
+    decrypt(message, shift)
 
 # alternative
 # def encrypt(message, shift):
