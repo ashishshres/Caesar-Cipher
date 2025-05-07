3e290b8286d9ee67cd64687c78bef8fc794cad78
Ashish Shrestha <ashishshr27@gmail.com>
2025-05-07 08:58:26 +0545

    initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+choice = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+message = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
