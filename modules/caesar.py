b8ed6c70304a9a705397d2b94e2814ff97230b1e
Ashish Shrestha <ashishshr27@gmail.com>
2025-05-08 12:07:07 +0545

    fix: handle for spaces

--- modules/caesar.py
+++ modules/caesar.py
@@ -3,16 +3,19 @@ alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n
 def caesar(text, method, shift):
   message = ""
   for char in text:
-    curr_position = alphabets.index(char)
-    if method == "encode":
-      shift_position = curr_position + shift 
-      if shift_position >= 25:
-        shift_position = shift_position % 26 
+    if char != " ":
+      curr_position = alphabets.index(char)
+      if method == "encode":
+        shift_position = curr_position + shift 
+        if shift_position >= 25:
+          shift_position = shift_position % 26 
+      else:
+        shift_position = curr_position - shift 
+        if shift_position < 0:
+          shift_position = shift_position + 26 
+    
+      message += alphabets[shift_position]
     else:
-      shift_position = curr_position - shift 
-      if shift_position < 0:
-        shift_position = shift_position + 26 
+      message += " "
     
-    message += alphabets[shift_position]
-    
-  print(f"The {method}d message is {message}")
+  print(f"The {method}d message is '{message}'")
