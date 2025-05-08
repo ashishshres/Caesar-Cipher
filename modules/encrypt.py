d2fd190e107268f1b0e24bdfae63d7598778e14d
Ashish Shrestha <ashishshr27@gmail.com>
2025-05-08 08:27:15 +0545

    add encrypt module

---
+++ modules/encrypt.py
@@ -0,0 +1,14 @@
+# def encrypt(plain_text, shift):
+#   # print(message)
+#   # print(shift)
+#   cipher_text = ""
+#   for char in plain_text:
+#     curr_position = alphabets.index(char)
+#     shift_position = curr_position + shift 
+#     if shift_position >= 25:
+#       shift_position = shift_position % 26 
+#     # print(shift_position)
+#     cipher_text += alphabets[shift_position]
+#   print(f"The encoded text is {cipher_text}")
+
+# encrypt(message, shift)
