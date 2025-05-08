fd5add9e2c78570d29aa08f65bed8ba773b4652d
Ashish Shrestha <ashishshr27@gmail.com>
2025-05-08 18:46:27 +0545

    refactor: improve user experience

--- main.py
+++ main.py
@@ -2,25 +2,32 @@ from modules.caesar import caesar
 from modules.logo import logo
 import os
 
-
+os.system("cls")
 should_continue = True
+
 while should_continue:
   logo()
   choice = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 
   if choice != "encode" and choice != "decode":
     print("Invalid choice!")
+    input("Press any key to continue..")
+    os.system("cls")
   else:
-      message = input("Type your message:\n").lower()
-      shift_amount = int(input("Type the shift number:\n"))
-      caesar(text = message, method = choice, shift = shift_amount)
-      go_again = input("Would you like to go again? Type 'yes' to continue or 'no' to exit:\n").lower()
-      os.system("cls")
+    message = input("Type your message:\n").lower()
+    shift_amount = int(input("Type the shift number:\n"))
+    caesar(text = message, method = choice, shift = shift_amount)
+    go_again = input("Would you like to go again? Type 'yes' to continue or 'no' to exit:\n").lower()
 
-      if go_again == "no":
-        should_continue = False
-        print("Goodbye! Thanks for using the Caesar Cipher tool.")
- 
+    if go_again != "no" and go_again != "yes":
+      print("Invalid choice!")
+      input("Press any key to continue..")
+      os.system("cls")
+    elif go_again == "no":
+      should_continue = False
+      print("Goodbye! Thanks for using the Caesar Cipher tool.")
+          
+    os.system("cls")
   # if choice == "encode":
   #   encrypt(message, shift)
   # else: 
