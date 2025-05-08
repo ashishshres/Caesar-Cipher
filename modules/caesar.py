f27c4b6ceecc6a572cfdf44f66e3a2edf80bb243
Ashish Shrestha <ashishshr27@gmail.com>
2025-05-08 08:30:23 +0545

    add caesar module

---
+++ modules/caesar.py
@@ -0,0 +1,18 @@
+alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(text, method, shift):
+  message = ""
+  for char in text:
+    curr_position = alphabets.index(char)
+    if method == "encode":
+      shift_position = curr_position + shift 
+      if shift_position >= 25:
+        shift_position = shift_position % 26 
+    else:
+      shift_position = curr_position - shift 
+      if shift_position < 0:
+        shift_position = shift_position + 26 
+    
+    message += alphabets[shift_position]
+    
+  print(f"The {method}d message is {message}")
